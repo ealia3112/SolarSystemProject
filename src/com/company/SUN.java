a59559636126c36cfcb08daafb79de549484a1c3
Elisa <ealia3112@gmail.com>
2022-08-27 13:07:02 -0400

    Feature SUN added but with error

---
+++ src/com/company/SUN.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+
+public class SUN {
+    
+
+    public static void main(String[] args) {
+        String name;
+        String color;
+        String radius;
+        String  temperature;
+
+        //    developing the feature SUN
+        SUN omaro=new SUN();
+        omaro.temperature="12334433kj";
+        omaro.name="ferro";
+        omaro.radius=12344555454545455454;
+        omaro.color="pink";
+    }
+    }
+
+
+
+}
